7a2840f135c9ab71c1635b9f27eae0d0263d8fa3
ljbroersen <lj.broersen@gmail.com>
2024-11-14 19:20:55 +0900

    feat: under the timer, there is now a flexbox with on the left a row for the current date and on the right a row for the timer logs

--- timer-diary/src/App.tsx
+++ timer-diary/src/App.tsx
@@ -70,81 +70,96 @@ export default function MyTimer({ expiryTimestamp }: MyTimerProps) {
 
   return (
     <>
-      <div className="text-center">
-        <h1>Timer Diary</h1>
-        {showInputs ? (
-          <div>
-            <input
-              type="number"
-              className="ml-2 mr-2 p-2"
-              placeholder="Hours"
-              value={customTime.hours}
-              onChange={(e) =>
-                setCustomTime({
-                  ...customTime,
-                  hours: e.target.value === "" ? 0 : parseInt(e.target.value),
-                })
-              }
-            />
-            <input
-              type="number"
-              className="ml-2 mr-2 p-2"
-              placeholder="Minutes"
-              value={customTime.minutes}
-              onChange={(e) =>
-                setCustomTime({
-                  ...customTime,
-                  minutes: e.target.value === "" ? 0 : parseInt(e.target.value),
-                })
-              }
-            />
-            <input
-              type="number"
-              className="ml-2 mr-2 p-2"
-              placeholder="Seconds"
-              value={customTime.seconds}
-              onChange={(e) =>
-                setCustomTime({
-                  ...customTime,
-                  seconds: e.target.value === "" ? 0 : parseInt(e.target.value),
-                })
-              }
-            />
-            <p className="m-2 mt-4">Description of activity</p>
-            <input
-              type="text"
-              className="ml-2 mr-2 p-2 w-4/5"
-              placeholder="What are you going to do?"
-              value={timerDescription}
-              onChange={(e) => setTimerDescription(e.target.value)}
-            />
+      <div className="flex flex-col h-screen">
+        <div className="flex-grow">
+          <div className="text-center">
+            <h1>Timer Diary</h1>
+            {showInputs ? (
+              <div>
+                <input
+                  type="number"
+                  className="ml-2 mr-2 p-2"
+                  placeholder="Hours"
+                  value={customTime.hours}
+                  onChange={(e) =>
+                    setCustomTime({
+                      ...customTime,
+                      hours:
+                        e.target.value === "" ? 0 : parseInt(e.target.value),
+                    })
+                  }
+                />
+                <input
+                  type="number"
+                  className="ml-2 mr-2 p-2"
+                  placeholder="Minutes"
+                  value={customTime.minutes}
+                  onChange={(e) =>
+                    setCustomTime({
+                      ...customTime,
+                      minutes:
+                        e.target.value === "" ? 0 : parseInt(e.target.value),
+                    })
+                  }
+                />
+                <input
+                  type="number"
+                  className="ml-2 mr-2 p-2"
+                  placeholder="Seconds"
+                  value={customTime.seconds}
+                  onChange={(e) =>
+                    setCustomTime({
+                      ...customTime,
+                      seconds:
+                        e.target.value === "" ? 0 : parseInt(e.target.value),
+                    })
+                  }
+                />
+                <p className="m-2 mt-4">Description of activity</p>
+                <input
+                  type="text"
+                  className="ml-2 mr-2 p-2 w-4/5"
+                  placeholder="What are you going to do?"
+                  value={timerDescription}
+                  onChange={(e) => setTimerDescription(e.target.value)}
+                />
 
-            <br />
-            <Button onClick={handleStart}>Start</Button>
+                <br />
+                <Button onClick={handleStart}>Start</Button>
+              </div>
+            ) : (
+              <div>
+                <div className="text-[100px]">
+                  <span>{hours.toString().padStart(2, "0")}</span>:
+                  <span>{minutes.toString().padStart(2, "0")}</span>:
+                  <span>{seconds.toString().padStart(2, "0")}</span>
+                </div>
+                <p>{isRunning ? "Running" : "Not running"}</p>
+                <Button onClick={pause}>Pause</Button>
+                <Button onClick={resume}>Resume</Button>
+                <Button onClick={handleRestart}>Restart</Button>
+              </div>
+            )}
           </div>
-        ) : (
-          <div>
-            <div className="text-[100px]">
-              <span>{hours.toString().padStart(2, "0")}</span>:
-              <span>{minutes.toString().padStart(2, "0")}</span>:
-              <span>{seconds.toString().padStart(2, "0")}</span>
+
+          <div className="flex flex-row mt-6">
+            <div className="flex flex-col w-3/12 mr-2">
+              <h2>Date</h2>
+              <p>Text</p>
+              <p>More text</p>
             </div>
-
-            <p>{isRunning ? "Running" : "Not running"}</p>
-            <Button onClick={pause}>Pause</Button>
-            <Button onClick={resume}>Resume</Button>
-            <Button onClick={handleRestart}>Restart</Button>
+            <div className="flex flex-col w-9/12 ml-2">
+              <h2>Log</h2>
+              <div className="flex flex-col">
+                {log.map((logItem) => (
+                  <div key={logItem} className="bg-zinc-900 p-2 m-2">
+                    <div dangerouslySetInnerHTML={{ __html: logItem }} />
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-        )}
-      </div>
-
-      <div className="text-center">
-        <h2>Log</h2>
-        {log.map((logItem) => (
-          <div key={logItem} className="bg-zinc-900 p-2 m-2">
-            <div dangerouslySetInnerHTML={{ __html: logItem }} />
-          </div>
-        ))}
+        </div>
       </div>
     </>
   );
