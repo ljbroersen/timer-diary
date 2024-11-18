4610ad42c3935842da128388794b726ceae0bd1d
ljbroersen <lj.broersen@gmail.com>
2024-11-18 22:18:24 +0900

    fix: .env.local

--- timer-diary/server/database.js
+++ timer-diary/server/database.js
@@ -1,11 +1,12 @@
 import { Pool } from "pg";
+require("dotenv").config({ path: "./.env.local" });
 
 const pool = new Pool({
   host: "localhost",
   port: 5432,
-  database: "timer_diary",
-  user: "postgres",
-  password: "maxwell",
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool;
