080f217ddda2ddf63b4d4eaa830dadc9c94e5567
ljbroersen <lj.broersen@gmail.com>
2024-12-07 12:45:01 +0900

    chore: refactored a little bit of code

--- knexfile.js
+++ knexfile.js
@@ -2,18 +2,18 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./server/.env.local" });
 
-const development = {
+export const development = {
   client: "pg",
   connection: {
     host: process.env.DB_HOST || "127.0.0.1",
-    port: Number(process.env.DB_PORT) || 5432,
+    port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME || "default_db",
     user: process.env.DB_USER || "default_user",
     password: process.env.DB_PASSWORD || "default_password",
   },
 };
 
-const production = {
+export const production = {
   client: "postgresql",
   connection: {
     database: process.env.PROD_DB_NAME || "my_db",
@@ -28,5 +28,3 @@ const production = {
     tableName: "knex_migrations",
   },
 };
-
-export { development, production };
